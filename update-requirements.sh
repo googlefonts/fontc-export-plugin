b78cc9e602c723992db6c304c916110ddb8393c2
Cosimo Lupo <clupo@google.com>
2025-11-24 11:20:08 +0000

    Automate fontc updates with webhook-based GitHub Actions workflow
    - Add auto-update workflow triggered by fontc release webhooks
    - Add update script that fetches latest release, updates files, and
    auto-increments versions
    - Add validation script with Python 3.11 venv testing to verify fontc
    installation
    
    For this to work automatically as soon as a new fontc is released, I'll also need to also set up a notification workflow in fontc repo.

--- update-requirements.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Download the requirements.txt file with pinned fontc version and SHA256 hashes
-# as specified in the 'VERSION' file.
-
-FONTC_REPO_URL="https://github.com/googlefonts/fontc"
-
-curl -LJo fontcExport.glyphsFileFormat/Contents/Resources/requirements.txt $FONTC_REPO_URL/releases/download/fontc-v$(<FONTC_VERSION)/requirements.txt
